8936e455c389bdd97db10c2553662dd6a3755702
yuehaiming <915873514@qq.com>
2018-12-05 22:30:32 +0800

    fix:文件替换

---
+++ src/components/Header.jsx
@@ -0,0 +1,12 @@
+/* 头部组件 */
+import React, { Component } from 'react';
+
+class Header extends Component{
+    render() {
+        return (
+            <div className="App-header"></div>
+        )
+    }
+}
+
+export default Header;
